0e36391ced55b1ed9d6bd295884a74f1b3fa824f
Kendall White <63067332+kendallw763@users.noreply.github.com>
2025-05-08 11:31:27 -0400

    Update Todo.jsx

--- frontend/my-app/src/Components/Todo.jsx
+++ frontend/my-app/src/Components/Todo.jsx
@@ -1,9 +1,30 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+
+function usePrevious(value){
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 function Todo(props) {
   const [isEditing, setEditing] = useState(false);
   const [newName, setNewName] = useState("");
+  const editFieldRef = useRef(null);
+  const editButtonRef = useRef(null);
+  const wasEditing = usePrevious(isEditing);
+  
+ 
 
+  useEffect(() => {
+    if (!wasEditing && isEditing) {
+      editFieldRef.current.focus();
+    } else if (wasEditing && !isEditing){
+      editButtonRef.current.focus();
+    }
+  }, [wasEditing, isEditing]);
+  
 
   function handleChange(e){
    setNewName(e.target.value);
@@ -28,6 +49,7 @@ function Todo(props) {
           type="text" 
           value={newName}
           onChange={handleChange}
+          ref={editFieldRef}
           />
         </div>
         <div className="btn-group">
@@ -58,7 +80,10 @@ function Todo(props) {
           </label>
         </div>
         <div className="btn-group">
-          <button type="button" className="btn" onClick={() => setEditing(true)}>
+          <button type="button"
+          className="btn" 
+          onClick={() => setEditing(true)}
+          ref={editButtonRef}>
             Edit <span className="visually-hidden">{props.name}</span>
           </button>
           <button
@@ -69,6 +94,7 @@ function Todo(props) {
           </button>
         </div>
       </div>
+      
     );
 
   
@@ -77,4 +103,5 @@ function Todo(props) {
   }
   
   export default Todo;
+  
   
